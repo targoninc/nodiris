1094b1bf2117903a53db166697de8a9a40031524
alexa <alex@targoninc.com>
2024-03-28 13:44:45 +0100

    add a basic server and start instructions

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to start
+
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm start`
+4. Open your browser and go to `http://localhost:3000/`
---
+++ server.mjs
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/ui')));
+
+app.listen(3000, () => {
+    console.log('Server listening at http://localhost:3000/');
+});
